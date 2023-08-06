6a88e07120e11c7f540b29a1b0f0d340a03b0a05
eliasekstedt <ekstedt.mbt@gmail.com>
2023-08-06 10:18:15 +0200

    reference run done, now trying without damage category

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
 
 def test0():
@@ -55,10 +56,44 @@ def test2():
     print(torch.mean(c1))
 
 def test3():
-    pass
+    trainsplit = pd.read_csv('csv/trainsplit.csv')
+    #testsplit = pd.read_csv('csv/save_testsplit.csv')
+    context1 = torch.tensor(trainsplit.iloc[0, 3:].tolist(), dtype=torch.float32)
+    #context2 = torch.tensor(testsplit.iloc[0, 2:].tolist(), dtype=torch.float32)
+    print(context1)
+    #print(context2)
+    #trainsplit = trainsplit[trainsplit.columns[~trainsplit.columns.str.startswith('damage')]]
+    #testsplit = testsplit[testsplit.columns[~testsplit.columns.str.startswith('damage')]]
+    #trainsplit.to_csv('csv/trainsplit.csv', index=False)
+    #testsplit.to_csv('csv/testsplit.csv', index=False)
+
 
 def test4():
-    pass
+    device = 'cuda'
+    path = {'trainmap':'csv/save_trainsplit.csv',
+            'testmap':'csv/save_testsplit.csv',
+            'valmap':'csv/Val.csv',
+            'data_labeled':'data/train/',
+            'data_unlabeled':'data/test/'
+            }
+    hparam = {'batch_size': 256,
+            'nr_epochs': 10,
+            'architecture_name':'res18fc',
+            'weight_decay': 1e-7,
+            'dropout_rate': 0.0,
+            'resizes':(128, 128),
+            'penalty':0}
+    
+
+    runpath = 'run/testing/06_08_18_53/'
+
+    from util.Networks import Res18FCNet
+    from util.Readers import Res18FCReader as Reader
+    testset = Reader(path['testmap'], path['data_labeled'], resizes=hparam['resizes'])
+    testloader = DataLoader(testset, batch_size=hparam['batch_size'], shuffle=False)
+    network = Res18FCNet(hparam['architecture_name'], hparam['weight_decay'], hparam['dropout_rate']).to(device)
+    from util.Evaluation_tools import create_pseudo_submission
+    create_pseudo_submission(network, runpath, testloader, device)
 
 def test5():
     pass
@@ -70,7 +105,9 @@ def test7():
     pass
 
 def main():
-    test2()
+    test4()
+    #test3()
+    #test2()
     #test1()
     #test0()
 
--- util/Evaluation_tools.py
+++ util/Evaluation_tools.py
@@ -4,6 +4,35 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
 import pandas as pd
+import time
+import torch
+
+def create_pseudo_submission(network, runpath, testloader, device):
+    tic = time.perf_counter()
+    network.load_state_dict(torch.load(runpath+'model.pth'))
+    preds, ids, fnames = None, None, None
+    network.eval()
+    with torch.no_grad():
+        for i, (batch_images, batch_context, batch_ids, batch_fnames) in enumerate(testloader):
+            if i%10 == 0:
+                print(i)
+            batch_images = batch_images.to(device)
+            batch_context = batch_context.to(device)
+            batch_outputs = network(batch_images, batch_context)
+            batch_outputs = tuple([el[0].item() for el in batch_outputs])
+            if preds is None and ids is None and fnames is None:
+                preds, ids, fnames = batch_outputs, batch_ids, batch_fnames
+            else:
+                preds = preds + batch_outputs
+                ids = ids + batch_ids
+                fnames = fnames + batch_fnames
+
+    submission = pd.DataFrame({'ID':ids, 'extent':preds})
+    submission['extent'] = submission['extent'].clip(lower=0, upper=100)
+    print(submission)
+    submission.to_csv('pseudofile.csv', index=False)
+    toc = time.perf_counter()
+    print(f'pseudofile created in {round(toc-tic, 2)} seconds\n')
 
 def create_assembly(path_extent, path_pred, runpath):
     df_extent = pd.read_csv(path_extent)
@@ -11,7 +40,7 @@ def create_assembly(path_extent, path_pred, runpath):
     df_pred.rename(columns={'extent':'pred'}, inplace=True)
     assembly = pd.concat([df_extent[['ID','filename','extent']], df_pred['pred']], axis=1)
     assembly['rpred'] = [round(el/5)*5 for el in assembly['pred']]
-    assembly.to_csv(runpath+'assembly.csv')
+    assembly.to_csv(runpath+'assembly.csv', index=False)
 
 
 def create_confusion_matrix(assembly, runpath):
