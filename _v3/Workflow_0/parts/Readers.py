c50a9a7798911b94c2dd82d103b8954c7792b1bc
eliasekstedt <ekstedt.mbt@gmail.com>
2023-08-27 15:53:07 +0200

    initiated v3

---
+++ _v3/Workflow_0/parts/Readers.py
@@ -0,0 +1,42 @@
+
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+
+
+from PIL import Image
+from torchvision import transforms
+
+class Reader(Dataset):
+    def __init__(self, path_csv, path_im, resizes, augment=False, eval=False):
+        transform = transforms.Compose([transforms.RandomHorizontalFlip()])
+        self.eval = eval
+        self.set = pd.read_csv(path_csv)
+        self.path_im = path_im
+        self.resizes = resizes
+        self.augment = augment
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.set)
+    
+    def __getitem__(self, idx):
+        row = self.set.iloc[idx]
+        name = row['filename']
+        image = Image.open(self.path_im+name)
+        image = self.make_size_uniform(image=image, size=self.resizes)
+        if self.eval:
+            id = row['ID']
+            return image, id, name
+        else:
+            label = torch.tensor([row['extent']], dtype=torch.float32)
+            if self.augment:
+                image = self.transform(image)
+            return image, label, name
+
+    def make_size_uniform(self, image, size):
+        resize = transforms.Compose([transforms.Resize(size), transforms.ToTensor()])
+        image = resize(image)
+        image.requires_grad_(True)
+        return image
